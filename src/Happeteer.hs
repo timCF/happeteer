57b8d0f8179f011532a8bb17c6585fe00ac6aa3b
tim2CF <tim4CF@gmail.com>
2018-09-09 23:55:20 +0300

    safeWaitForProcess function

--- src/Happeteer.hs
+++ src/Happeteer.hs
@@ -1,10 +1,74 @@
 module Happeteer
-    ( someFunc
+    ( scrap
     ) where
 
+import           Control.Concurrent
 import           Control.Monad
+import           GHC.IO.Handle
+import           System.Exit
 import           System.Process
 
-someFunc :: IO ()
-someFunc =
-  void . createProcess . shell $ "echo 'hello'"
+data Scraped = Scraped {
+  exitCode :: ExitCode,
+  stdOut   :: String,
+  stdErr   :: String
+} deriving (Show)
+
+data TimeMS = TimeMS Integer deriving (Eq, Ord, Show)
+instance Num TimeMS where
+  TimeMS t1 + TimeMS t2 = TimeMS $ t1 + t2
+  TimeMS t1 - TimeMS t2 = TimeMS $ t1 - t2
+  TimeMS t1 * TimeMS t2 = TimeMS $ t1 * t2
+  abs (TimeMS t) = TimeMS $ abs t
+  signum (TimeMS t)
+    | t == 0 = TimeMS t
+    | t < 0 = TimeMS (-1)
+    | otherwise = TimeMS 1
+  fromInteger = TimeMS
+
+-- TimeMS type utils
+sleepMS :: TimeMS -> IO ()
+sleepMS (TimeMS t) =
+  threadDelay $ fromIntegral t
+
+-- Wait for process 1 min and if no exit - terminate it
+safeWaitForProcess :: ProcessHandle -> TimeMS -> IO ExitCode
+safeWaitForProcess ph elapsed_time
+  | elapsed_time > processLimit = do
+    _ <- terminateProcess ph
+    mb_exit_code <- getProcessExitCode ph
+    case mb_exit_code of
+      Just _ ->
+        return $ ExitFailure 504
+      Nothing -> do
+        _ <- sleepMS processInterval
+        safeWaitForProcess ph elapsed_time
+  | otherwise = do
+    mb_exit_code <- getProcessExitCode ph
+    case mb_exit_code of
+      Just exit_code ->
+        return exit_code
+      Nothing        -> do
+        _ <- sleepMS processInterval
+        safeWaitForProcess ph (elapsed_time + processInterval)
+  where
+    processLimit = TimeMS 60000000
+    processInterval = TimeMS 500000
+
+scrap :: IO Scraped
+scrap =
+  let
+    process_spec = (shell "sleep 10; echo hello"){
+      std_out = CreatePipe,
+      std_err = CreatePipe
+    }
+  in do
+    (Nothing, Just std_out_h, Just std_err_h, ph) <- createProcess process_spec
+    exit_code <- safeWaitForProcess ph (TimeMS 0)
+    std_out <- hGetContents std_out_h
+    std_err <- hGetContents std_err_h
+    return Scraped{
+      exitCode = exit_code,
+      stdOut   = std_out,
+      stdErr   = std_err
+    }
