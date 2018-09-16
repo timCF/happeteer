5d47942487962a09f9b549bec7a9c1c57d5a6a37
tim2CF <tim4CF@gmail.com>
2018-09-17 00:44:09 +0300

    scrapURLTest works

--- test/HappeteerTest.hs
+++ test/HappeteerTest.hs
@@ -1,30 +1,36 @@
 module Main where
 
-import           Network.URL
-import           System.Exit
+import           Network.URL      (URL, importURL)
+import           System.Exit      (ExitCode (ExitSuccess))
 
-import           Test.Tasty
-import           Test.Tasty.HUnit
+import           Test.Tasty       (TestTree, defaultMain, testGroup)
+import           Test.Tasty.HUnit (assertEqual, testCase)
 
-import           Happeteer
+import           Happeteer        (AbstractScraped (AbstractScraped, exitCode, stdErr, stdOut),
+                                   Scraped (Scraped, abstract, content),
+                                   scrapURL)
 
 main :: IO ()
-main =
-  defaultMain (testGroup "Happeteer Tests" [scrapURLTest])
+main = do
+  actual_scraped_url <- scrapURL raw_url
+  defaultMain (testGroup "Happeteer Tests" [scrapURLTest actual_scraped_url])
+  where
+    Just raw_url = importURL "http://google.com"
 
-scrapURLTest :: TestTree
-scrapURLTest = testCase "Testing scrapURL"
-    (assertEqual "Follow redirect google.com" expected_result (scrapURL raw_url))
-    where
-      Just raw_url = importURL "http://google.com"
-      Just expected_url = importURL "https://www.google.com"
-      expected_result :: IO(Scraped URL)
-      expected_result =
-        return Scraped{
-          content = Right expected_url,
-          abstract = AbstractScraped {
-            exitCode = ExitSuccess,
-            stdOut   = "https://www.google.com",
-            stdErr   = ""
-          }
+scrapURLTest :: Scraped URL -> TestTree
+scrapURLTest actual_scraped_url =
+  testCase "Testing scrapURL" (assertEqual "Follow redirect google.com" expected_result actual_scraped_url)
+  where
+    expected_std_out =
+      "https://www.google.com/?gws_rd=ssl"
+    Just expected_url =
+      importURL expected_std_out
+    expected_result =
+      Scraped{
+        content = Right expected_url,
+        abstract = AbstractScraped {
+          exitCode = ExitSuccess,
+          stdOut   = expected_std_out,
+          stdErr   = ""
         }
+      }
