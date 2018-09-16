d62a7a65166f9fd0ebb6f976bec83996453797f4
tim2CF <tim4CF@gmail.com>
2018-09-16 14:49:20 +0300

    modern-uri -> url

--- src/Happeteer.hs
+++ src/Happeteer.hs
@@ -1,22 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Happeteer
-    ( resolveURI
+    ( resolveURL
     ) where
 
 import qualified Control.Concurrent (threadDelay)
-import qualified Data.Text          (pack, strip)
+import qualified Data.Text          (pack, strip, unpack)
 import qualified GHC.IO.Handle      (hGetContents)
+import qualified Network.URL        (URL, exportURL, importURL)
 import qualified System.Exit        (ExitCode,
                                      ExitCode (ExitFailure, ExitSuccess))
 import qualified System.Process     (CreateProcess (..), ProcessHandle,
                                      StdStream (CreatePipe), createProcess,
                                      getProcessExitCode, proc, terminateProcess)
-import qualified Text.URI           (URI, mkURI, renderStr)
-
--- import qualified Control.Monad.Catch (MonadThrow (..))
--- import qualified Data.Text           (Text)
--- {-# SPECIALIZE Text.URI.mkURI :: Control.Monad.Catch.MonadThrow m0 => Data.Text.Text -> m0 Text.URI.URI #-}
 
 data AbstractScraped = AbstractScraped {
   exitCode :: System.Exit.ExitCode,
@@ -29,33 +25,17 @@ data Scraped content = Scraped {
   abstract :: AbstractScraped
 } deriving (Show)
 
-data TimeMS = TimeMS Integer deriving (Eq, Ord, Show)
-instance Num TimeMS where
-  TimeMS t1 + TimeMS t2 = TimeMS $ t1 + t2
-  TimeMS t1 - TimeMS t2 = TimeMS $ t1 - t2
-  TimeMS t1 * TimeMS t2 = TimeMS $ t1 * t2
-  abs (TimeMS t) = TimeMS $ abs t
-  signum (TimeMS t)
-    | t == 0 = TimeMS t
-    | t < 0 = TimeMS (-1)
-    | otherwise = TimeMS 1
-  fromInteger = TimeMS
-
 -- resolve URL (follow redirect etc)
-resolveURI :: Text.URI.URI -> IO (Scraped Text.URI.URI)
-resolveURI uri = do
-  abstract_scraped <- scrap "js/resolve-uri.js" [Text.URI.renderStr uri]
+resolveURL :: Network.URL.URL -> IO (Scraped Network.URL.URL)
+resolveURL url = do
+  abstract_scraped <- scrap "js/resolve-url.js" [Network.URL.exportURL url]
   case abstract_scraped of
     AbstractScraped{
       exitCode = System.Exit.ExitSuccess,
       stdOut   = std_out
-    } -> do
-      --
-      --  TODO : Text.URI.mkURI can throw exception here!!!
-      --
-      result_uri <- Text.URI.mkURI $ Data.Text.strip $ Data.Text.pack std_out
+    } ->
       return Scraped{
-        content  = Just result_uri,
+        content  = Network.URL.importURL $ Data.Text.unpack $ Data.Text.strip $ Data.Text.pack std_out,
         abstract = abstract_scraped
       }
     AbstractScraped{} ->
@@ -64,13 +44,12 @@ resolveURI uri = do
         abstract = abstract_scraped
       }
 
--- TimeMS type utils
-sleepMS :: TimeMS -> IO ()
-sleepMS (TimeMS t) =
+sleepMS :: Integer -> IO ()
+sleepMS t =
   Control.Concurrent.threadDelay $ fromIntegral t
 
 -- Wait for process 1 min and if no exit - terminate it
-safeWaitForProcess :: System.Process.ProcessHandle -> TimeMS -> IO System.Exit.ExitCode
+safeWaitForProcess :: System.Process.ProcessHandle -> Integer -> IO System.Exit.ExitCode
 safeWaitForProcess ph elapsed_time
   | elapsed_time > processLimit = do
     _ <- System.Process.terminateProcess ph
@@ -90,8 +69,10 @@ safeWaitForProcess ph elapsed_time
         _ <- sleepMS processInterval
         safeWaitForProcess ph (elapsed_time + processInterval)
   where
-    processLimit = TimeMS 60000000
-    processInterval = TimeMS 500000
+    processLimit :: Integer
+    processLimit = 60000000
+    processInterval :: Integer
+    processInterval = 500000
 
 -- abstract scraping of something
 scrap :: String -> [String] -> IO AbstractScraped
@@ -104,7 +85,7 @@ scrap script params =
 
   in do
     (Nothing, Just std_out_h, Just std_err_h, ph) <- System.Process.createProcess process_spec
-    exit_code <- safeWaitForProcess ph (TimeMS 0)
+    exit_code <- safeWaitForProcess ph 0
     std_out <- GHC.IO.Handle.hGetContents std_out_h
     std_err <- GHC.IO.Handle.hGetContents std_err_h
     return AbstractScraped{
